f69ccf2ef450b1385c9c2b13911df0e6a1ba86c9
David Puleri <david@puleri.com>
2015-04-06 10:22:47 +0200

    Created startup script

---
+++ MycoachApp/restart-container.php
@@ -0,0 +1,28 @@
+<?php
+
+exec("docker ps | grep mycoachfootball_web | awk '{print $1}'",$runningProcess, $returnValue);
+foreach($runningProcess as $processToRestart){
+    echo 'Restarting container';
+    exec('docker restart '.$processToRestart,$output);
+}
+
+if(0 == count($runningProcess)){
+    echo 'No process found';
+    exec('docker rm mycoachfootball_web');
+
+    $workspace = $argv[1];
+    $baseLogFolder = $argv[2];
+
+    $cmd='docker run -d -p "1234:80" ' .
+        '-v "'.$workspace.':/var/www/html" ' .
+        '-v "'.$baseLogFolder.':/var/log/nginx" ' .
+        '-v "'.getcwd().'/host:/etc/nginx/sites-enabled" ' .
+        '--link mycoachfootball_db:mysql ' .
+        '--name mycoachfootball_web ' .
+        'docker-registry.mycoachfootball.com:5000/global/nginx';
+
+    exec($cmd);
+
+
+
+}
